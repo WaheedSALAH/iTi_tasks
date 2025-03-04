4c0bab061f22f402fd32faa9a450c599029b0064
waheedSALAH <w55h44@gmail.com>
2025-03-04 20:32:01 +0200

    done

---
+++ lab 3/func.js
@@ -0,0 +1,8 @@
+// let img = document.getElementsByTagName('img');
+
+// for (let i = 0; i < img.length; i++) {
+    
+//     img[i].addEventListener('click', function() {
+//         img[i].style.scale = 3;
+//     });
+// }
